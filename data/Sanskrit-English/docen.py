f91a54098232035a222fff0549c3278f4cd51fe0
adityashar <adityas29@outlook.com>
2020-04-04 12:29:54 +0530

    TL2

---
+++ data/Sanskrit-English/docen.py
@@ -0,0 +1,47 @@
+import re
+
+location = "en-sa.en.all"
+req = open(location, 'r', encoding="utf-8").read()
+
+#req = [re.sub(r'\(.*\)', '', line.rstrip()) for line in file]
+
+text = re.sub('<[^<]+>', "", req)
+text1=text.split('\n')[:-1]
+non_empty_lines = [line for line in text1 if line.strip() != ""]
+text1=""
+article=""
+list1=[]
+for i in range(len(non_empty_lines)):
+    ans=non_empty_lines[i]
+    article = re.sub(r'\([^)]*\)', r'', ans)
+    article = re.sub(r'\[[^\]]*\]', r'', article)
+    article = re.sub(r'<[^>]*>', r'', article)
+    article = re.sub(r'^https?:\/\/.*[\r\n]*', '', article)
+    article = article.replace(u'\ufeff','')
+    article = article.replace(u'\xa0', u' ')
+    article = article.replace('  ', ' ')
+    article = article.replace(' , ', ', ')
+    
+    text1+=article+'\n'
+    article = re.sub('[0-9]', '', article)
+    list1.append(article.lstrip())
+
+#print(data[:100])
+
+train = list1[:int(0.85*len(list1))]
+valid = list1[int(0.85*len(list1)) + 1: int(0.9*len(list1))]
+test = list1[int(0.9*len(list1)) + 1:]
+
+print(len(train), len(valid), len(test))
+
+
+def write(file, data):
+    with open(file, 'w', encoding="utf-8") as f:
+        for item in data:
+            f.write("%s\n" % item)
+
+write("en-sa.en.train", train)
+write("en-sa.en.valid", valid)
+write("en-sa.en.test", test)
+write("en-sa", list1)
+
---
+++ data/Sanskrit-English/docsn.py
@@ -0,0 +1,64 @@
+#from cltk.tokenize.sentence import TokenizeSentence as TS
+import re
+from indicnlp.tokenize import indic_tokenize  
+
+
+#tokenizer = TS('sanskrit')
+
+location = "OpenSubtitles.en-sa.sa"
+
+req = open(location, 'r', encoding="utf-8").read()
+
+#req = [re.sub(r'\(.*\)', '', line.rstrip()) for line in file]
+
+text = re.sub('<[^<]+>', "", req)
+text1=text.split('\n')[:-1]
+non_empty_lines = [line for line in text1 if line.strip() != ""]
+text1=""
+article=""
+list1=[]
+for i in range(len(non_empty_lines)):
+    ans=non_empty_lines[i]
+    article = re.sub(r'\([^)]*\)', r'', ans)
+    article = re.sub(r'\[[^\]]*\]', r'', article)
+    article = re.sub(r'<[^>]*>', r'', article)
+    article = re.sub(r'^https?:\/\/.*[\r\n]*', '', article)
+    article = article.replace(u'\ufeff','')
+    article = article.replace(u'\xa0', u' ')
+    article = article.replace('  ', ' ')
+    article = article.replace(' , ', ', ')
+    devanagari_nums = ('०','१','२','३','४','५','६','७','८','९')
+    for c, n in enumerate(devanagari_nums):
+        article = re.sub(n, str(c), article)
+    text1+=article+'\n'
+    article = re.sub('[0-9]', '', article)
+    article = article.lstrip()
+    #print(type(article))	
+    tok_line = indic_tokenize.trivial_tokenize(article)
+    string = ' '.join(tok_line)	
+    list1.append(string)
+    
+
+tokenized_data = list1
+
+#for line in data[:100]:
+ #   print(line)
+  #  tok_line = tokenizer.tokenize(line)
+   # tokenized_data.append(tok_line)
+
+train = tokenized_data[:int(0.85*len(tokenized_data))]
+valid = tokenized_data[int(0.85*len(tokenized_data)) + 1: int(0.9*len(tokenized_data))]
+test = tokenized_data[int(0.9*len(tokenized_data)) + 1:]
+
+print(len(train), len(valid), len(test))
+
+
+def write(file, data):
+    with open(file, 'w', encoding="utf-8") as f:
+        for item in data:
+            f.write("%s\n" % item)
+
+write("en-sa.sa.train", train)
+write("en-sa.sa.valid", valid)
+write("en-sa.sa.test", test)
+write("en-sa.sa.all", tokenized_data)
